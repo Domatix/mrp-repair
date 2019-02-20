4b48451f4ff27e83745050fe6c17e2de56272c3a
Catalin <c>
2019-02-20 11:45:33 +0100

    added more tests for extra coverage

--- mrp_repair_resp/tests/test_repair.py
+++ mrp_repair_resp/tests/test_repair.py
@@ -17,9 +17,17 @@ class TestRepair(TransactionCase):
             'date_start': fields.Datetime.from_string("2019-02-18 10:19:01"),
             'date_end': fields.Datetime.from_string("2019-02-18 11:19:01")
         })
-
+        self.time_ids03 = self.time_ids_demiurg.create({
+            'date_start': fields.Datetime.from_string("2019-02-20 11:36:01"),
+            'date_end': fields.Datetime.from_string("2019-02-20 11:35:01")
+        })
+        self.time_ids04 = self.time_ids_demiurg.create({
+            'date_start': fields.Datetime.from_string("2019-02-20 11:36:01"),
+            'date_end': fields.Datetime.from_string("2019-02-20 11:36:01")
+        })
         self.repair01 = self.env.ref('mrp_repair.mrp_repair_rmrp0')
         self.repair02 = self.env.ref('mrp_repair.mrp_repair_rmrp1')
+        self.repair03 = self.env.ref('mrp_repair.mrp_repair_rmrp2')
 
     def test_duration_repair01(self):
         self.repair01.time_ids = [(4, self.time_ids01.id)]
@@ -32,3 +40,9 @@ class TestRepair(TransactionCase):
         self.repair02.time_ids = [
             (6, 0, [self.time_ids01.id, self.time_ids02.id])]
         self.assertEqual(len(self.repair02.time_ids), 2)
+
+    def test_date_start(self):
+        self.repair02.time_ids = [
+            (6, 0, [self.time_ids04.id, self.time_ids03.id])]
+
+        self.assertEqual(len(self.repair02.time_ids), 0)
